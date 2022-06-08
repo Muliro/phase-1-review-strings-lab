c8a8e27392b8acccd29dc82e9ee0d6732195f32d
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-08 08:47:22 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'; //declaring the currentuser variable
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; //declaring the welcomeMessage variable
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(); //declaring the excitedWelcomeMessage variable
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`; //declaring a new variable and slicing to only return its first char
+
